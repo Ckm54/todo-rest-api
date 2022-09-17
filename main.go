79dd947b9610a58e74c9cfb4bea665a0e1283c63
collo <mutaic84@gmail.com>
2022-09-17 22:50:27 +0300

    setup api routes

--- main.go
+++ main.go
@@ -44,6 +44,19 @@ func getTodo(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todo)
 }
 
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+	
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func getTodoById(id string) (*Todo, error){
 	for i, t := range todos {
 		if t.ID == id {
@@ -57,6 +70,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
